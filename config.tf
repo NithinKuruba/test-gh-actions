382beaef6a80d068804a2b2f05d4feceb318c362
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2023-02-15 15:24:58 -0800

    chore: new action to emit notifs

--- config.tf
+++ config.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 0.15.5"
 
+  backend "s3" {
+    bucket         = "abc123"
+    key            = "kc/gold"
+    dynamodb_table = "abc123-state-locking"
+    region         = "ca-central-1"
+  }
+
   required_providers {
     keycloak = {
       source  = "mrparkers/keycloak"
